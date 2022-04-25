83baa8bb0276c3db1c6469591daf600ac5d44525
Sarah Moss <sarah-moss@live.com>
2022-04-25 01:20:15 +0000

    Complete stdin setup + add handleUserInput

--- play.js
+++ play.js
@@ -1,1 +1,22 @@
 const { connect } = require("./client");
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+
+}
+};
+
+setupInput();
